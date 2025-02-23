871350e4924bae8636d24d286f3dc3fe12ac5048
sergiosantosjr94 <sllsjunior@gmail.com>
2025-02-23 00:16:42 -0300

    feat: ensure cache is revalidated when creating an order

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/prisma";
 
 import { removeCpfPunctuation } from "../helpers/cpf";
+import { revalidatePath } from "next/cache";
 
 interface CreateOrderInput {
   customerName: string;
@@ -60,5 +61,6 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  revalidatePath(`/${input.slug}/orders`);
   redirect(`/${input.slug}/orders/?cpf=${input.customerCpf}`);
 };
